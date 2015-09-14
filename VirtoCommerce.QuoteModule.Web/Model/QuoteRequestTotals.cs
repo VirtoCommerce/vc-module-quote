0b70274a27801937be08261c0c49466bf76383e9
E.Tatarincev <et@virtoway.com>
2015-09-14 17:56:21 +0200

    Quotes calculation totals function and support manual subtotal (not finished Tax and Coupon discount calculation todo)

--- VirtoCommerce.QuoteModule.Web/Model/QuoteRequestTotals.cs
+++ VirtoCommerce.QuoteModule.Web/Model/QuoteRequestTotals.cs
@@ -8,15 +8,29 @@ namespace VirtoCommerce.QuoteModule.Web.Model
     public class QuoteRequestTotals
     {
         /// <summary>
-        /// Grand total subtotal + shipping - discount + tax
+        /// Original subtotal tier quantity * sale price 
         /// </summary>
-        public decimal Total { get; set; }
+        public decimal OriginalSubTotalExlTax { get; set; }
         /// <summary>
         /// Items proposal tier quantity * proposal price
         /// </summary>
-        public decimal SubTotal { get; set; }
+        public decimal SubTotalExlTax { get; set; }
         public decimal ShippingTotal { get; set; }
         public decimal DiscountTotal { get; set; }
         public decimal TaxTotal { get; set; }
+
+        /// <summary>
+        /// Adjustment SubTotalOriginalExlTax -  SubTotalExlTax
+        /// </summary>
+        public decimal AdjustmentQuoteExlTax { get; set; }
+
+        /// <summary>
+        /// Grand total SubTotalExlTax + shipping - discount
+        /// </summary>
+        public decimal GrandTotalExlTax { get; set; }
+        /// <summary>
+        /// Grand total subtotal + shipping - discount + tax
+        /// </summary>
+        public decimal GrandTotalInclTax { get; set; }
     }
 }
