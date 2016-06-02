4669bb42bb1ca2891380aeb984cb5ba8c9cd597b
artem-dudarev <ad@virtoway.com>
2016-06-02 14:50:24 +0200

    Added VirtoCommerce.QuoteModule.Client

--- NuGet/publish.bat
+++ NuGet/publish.bat
@@ -1,3 +1,4 @@
-set V=2.10.3
+set V=2.10.4
+nuget push VirtoCommerce.QuoteModule.Client.%V%.nupkg -Source nuget.org -ApiKey %1
 nuget push VirtoCommerce.QuoteModule.Data.%V%.nupkg -Source nuget.org -ApiKey %1
 pause
