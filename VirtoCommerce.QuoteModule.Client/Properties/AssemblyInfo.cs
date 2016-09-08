7d91036a61ad50aa712f9f083284ffacb945e7c9
eMazeika <em@virtoway.com>
2016-09-08 16:50:32 +0300

    Client project deleted

--- VirtoCommerce.QuoteModule.Client/Properties/AssemblyInfo.cs
+++
@@ -1,4 +0,0 @@
-using System.Reflection;
-
-[assembly: AssemblyTitle("VirtoCommerce.QuoteModule.Client")]
-[assembly: AssemblyDescription("VirtoCommerce.Quote API client generated from a Swagger doc")]
