beb92515c2513d423846ea76c3026538f1241580
Yuliya Malik <yuliya.umbrellagroup@gmail.com>
2018-03-28 19:24:46 +0300

    Fixed test styles

--- VirtoCommerce.QuoteModule.Test/QuoteModuleTests.cs
+++ VirtoCommerce.QuoteModule.Test/QuoteModuleTests.cs
@@ -26,119 +26,164 @@ namespace VirtoCommerce.QuoteModule.Test
     public class QuoteModuleTests
     {
         [Fact]
-        public void TierPrice_Types_Converting()
+        public void TierPrice_Types_Converting_ReturnsTypes()
         {
+            //Arrange
             var tierPriceData = new dataModel.TierPriceEntity();
             var tierPriceDomain = new domainModel.TierPrice();
             var tierPriceWeb = new webModel.TierPrice();
 
+            //Act
             var from1 = tierPriceData.FromModel(tierPriceDomain);
+            //Assert
             Assert.IsType<dataModel.TierPriceEntity>(from1);
 
+            //Act
             var to1 = tierPriceData.ToModel(tierPriceDomain);
+            //Assert
             Assert.IsType<domainModel.TierPrice>(to1);
 
+            //Act
             var toWeb1 = TierPriceConverter.ToWebModel(tierPriceDomain);
+            //Assert
             Assert.IsType<webModel.TierPrice>(toWeb1);
 
+            //Act
             tierPriceWeb = toWeb1;
             var toCore1 = TierPriceConverter.ToCoreModel(tierPriceWeb);
+            //Assert
             Assert.IsType<domainModel.TierPrice>(toCore1);
         }
 
         [Fact]
-        public void QuoteItem_Types_Converting()
+        public void QuoteItem_Types_Converting_ReturnsTypes()
         {
+            //Arrange
             var pkMap = new PrimaryKeyResolvingMap();
             //Testing transformation of QuoteItem types
             var quoteItemData = new dataModel.QuoteItemEntity();
             var quoteItemDomain = new domainModel.QuoteItem();
             var quoteItemWeb = new webModel.QuoteItem();
 
+            //Act
             var to2 = quoteItemData.ToModel(quoteItemDomain);
+            //Assert
             Assert.IsType<domainModel.QuoteItem>(to2);
 
+            //Act
             var from2 = quoteItemData.FromModel(quoteItemDomain, pkMap);
+            //Assert
             Assert.IsType<dataModel.QuoteItemEntity>(from2);
 
+            //Act
             var toWeb2 = Web.Converters.QuoteItemConverter.ToWebModel(quoteItemDomain);
+            //Assert
             Assert.IsType<webModel.QuoteItem>(toWeb2);
 
+            //Act
             quoteItemWeb = toWeb2;
             var toCore2 = Web.Converters.QuoteItemConverter.ToCoreModel(quoteItemWeb);
+            //Assert
             Assert.IsType<domainModel.QuoteItem>(toCore2);
         }
 
         [Fact]
-        public void QuoteRequest_Types_Converting()
+        public void QuoteRequest_Types_Converting_ReturnsTypes()
         {
+            //Arrange
             var pkMap = new PrimaryKeyResolvingMap();
 
             var quoteRequestData = new dataModel.QuoteRequestEntity();
             var quoteRequestDomain = new domainModel.QuoteRequest();
             var quoteRequestWeb = new webModel.QuoteRequest();
 
+            //Act
             var to3 = quoteRequestData.ToModel(quoteRequestDomain);
+            //Assert
             Assert.IsType<domainModel.QuoteRequest>(to3);
 
             var from3 = quoteRequestData.FromModel(quoteRequestDomain, pkMap);
+            //Assert
             Assert.IsType<dataModel.QuoteRequestEntity>(from3);
 
+            //Act
             var toWeb3 = Web.Converters.QuoteRequestConverter.ToWebModel(quoteRequestDomain);
+            //Assert
             Assert.IsType<webModel.QuoteRequest>(toWeb3);
 
+            //Act
             quoteRequestWeb = toWeb3;
             var toCore3 = Web.Converters.QuoteRequestConverter.ToCoreModel(quoteRequestWeb);
+            //Assert
             Assert.IsType<domainModel.QuoteRequest>(toCore3);
         }
 
         [Fact]
-        public void Attachment_Types_Converting()
+        public void Attachment_Types_Converting_ReturnsTypes()
         {
+            //Arrange
             var pkMap = new PrimaryKeyResolvingMap();
 
             var attachmentData = new dataModel.AttachmentEntity();
             var attachmentDomain = new domainModel.QuoteAttachment();
             var attachmentWeb = new webModel.QuoteAttachment();
 
+            //Act
             var to4 = attachmentData.ToModel(attachmentDomain);
+            //Assert
             Assert.IsType<domainModel.QuoteAttachment>(to4);
 
+            //Act
             var from4 = attachmentData.FromModel(attachmentDomain, pkMap);
+            //Assert
             Assert.IsType<dataModel.AttachmentEntity>(from4);
 
+            //Act
             var toWeb4 = QuoteAttachmentConverter.ToWebModel(attachmentDomain);
+            //Assert
             Assert.IsType<webModel.QuoteAttachment>(toWeb4);
 
+            //Act
             attachmentWeb = toWeb4;
             var toCore4 = QuoteAttachmentConverter.ToCoreModel(attachmentWeb);
+            //Assert
             Assert.IsType<domainModel.QuoteAttachment>(toCore4);
         }
 
         [Fact]
-        public void Address_Types_Converting()
+        public void Address_Types_Converting_ReturnsTypes()
         {
+            //Arrange
             var addressData = new dataModel.AddressEntity();
             var addressDomain = new Address();
             var addressWeb = new webModel.Address();
 
+            //Act
             var from5 = addressData.FromModel(addressDomain);
+            //Assert
             Assert.IsType<dataModel.AddressEntity>(from5);
 
+            //Act
             var to5 = addressData.ToModel(addressDomain);
+            //Assert
             Assert.IsType<Address>(to5);
 
+            //Act
             var toWeb5 = AddressConverter.ToWebModel(addressDomain);
+            //Assert
             Assert.IsType<webModel.Address>(toWeb5);
 
+            //Act
             addressWeb = toWeb5;
             var toCore5 = AddressConverter.ToCoreModel(addressWeb);
+            //Assert
             Assert.IsType<Address>(toCore5);
         }
 
         [Fact]
-        public void QuoteRequest_ShipmentMethod_Converting()
+        public void QuoteRequestShipmentMethod_Converting_ReturnsShipmentMethod()
         {
+            //Arrange
             var pkMap = new PrimaryKeyResolvingMap();
             var quoteRequestData = new dataModel.QuoteRequestEntity();
             quoteRequestData.ShipmentMethodCode = "123123";
@@ -150,27 +195,36 @@ namespace VirtoCommerce.QuoteModule.Test
             var quoteRequestWeb = new webModel.QuoteRequest();
             quoteRequestWeb.ShipmentMethod = new webModel.ShipmentMethod();
 
+            //Act
             var to3 = quoteRequestData.ToModel(quoteRequestDomain);
+            //Assert
             Assert.Equal("123123", to3.ShipmentMethod.ShipmentMethodCode);
             Assert.Equal("TestName", to3.ShipmentMethod.OptionName);
 
+            //Act
             var from3 = quoteRequestData.FromModel(quoteRequestDomain, pkMap);
+            //Assert
             Assert.Equal("123123", from3.ShipmentMethodCode);
             Assert.Equal("TestName", from3.ShipmentMethodOption);
 
+            //Act
             var toWeb3 = Web.Converters.QuoteRequestConverter.ToWebModel(quoteRequestDomain);
+            //Assert
             Assert.Equal("123123", toWeb3.ShipmentMethod.ShipmentMethodCode);
             Assert.Equal("TestName", toWeb3.ShipmentMethod.OptionName);
 
+            //Act
             quoteRequestWeb = toWeb3;
             var toCore3 = Web.Converters.QuoteRequestConverter.ToCoreModel(quoteRequestWeb);
+            //Assert
             Assert.Equal("123123", toCore3.ShipmentMethod.ShipmentMethodCode);
             Assert.Equal("TestName", toCore3.ShipmentMethod.OptionName);
         }
 
         [Fact]
-        public void InheritFromAuditableEntity()
+        public void InheritFromAuditableEntity_ReturnsCreatedDate()
         {
+            //Arrange
             var pkMap = new PrimaryKeyResolvingMap();
 
             //Testing transformation CreatedDate of QuoteRequest types
@@ -180,43 +234,61 @@ namespace VirtoCommerce.QuoteModule.Test
             var quoteRequestDomain = new domainModel.QuoteRequest();
             var quoteRequestWeb = new webModel.QuoteRequest();
 
+            //Act
             var to3 = quoteRequestData.ToModel(quoteRequestDomain);
+            //Assert
             Assert.Equal(DateTime.Now.Date, to3.CreatedDate);
 
+            //Act
             var from3 = quoteRequestData.FromModel(quoteRequestDomain, pkMap);
+            //Assert
             Assert.Equal(DateTime.Now.Date, from3.CreatedDate);
 
+            //Act
             var toWeb3 = Web.Converters.QuoteRequestConverter.ToWebModel(quoteRequestDomain);
+            //Assert
             Assert.Equal(DateTime.Now.Date, toWeb3.CreatedDate);
 
+            //Act
             quoteRequestWeb = toWeb3;
             var toCore3 = Web.Converters.QuoteRequestConverter.ToCoreModel(quoteRequestWeb);
+            //Assert
             Assert.Equal(DateTime.Now.Date, toCore3.CreatedDate);
 
             //Testing transformation CreatedDate of QuoteAttachment types
+            //Arrange
             var attachmentData = new dataModel.AttachmentEntity();
             attachmentData.CreatedDate = DateTime.Now.Date;
 
             var attachmentDomain = new domainModel.QuoteAttachment();
             var attachmentWeb = new webModel.QuoteAttachment();
 
+            //Act
             var to4 = attachmentData.ToModel(attachmentDomain);
+            //Assert
             Assert.Equal(DateTime.Now.Date, to4.CreatedDate);
 
+            //Act
             var from4 = attachmentData.FromModel(attachmentDomain, pkMap);
+            //Assert
             Assert.Equal(DateTime.Now.Date, from4.CreatedDate);
 
+            //Act
             var toWeb4 = QuoteAttachmentConverter.ToWebModel(attachmentDomain);
+            //Assert
             Assert.Equal(DateTime.Now.Date, toWeb4.CreatedDate);
 
+            //Act
             attachmentWeb = toWeb4;
             var toCore4 = QuoteAttachmentConverter.ToCoreModel(attachmentWeb);
+            //Assert
             Assert.Equal(DateTime.Now.Date, toCore4.CreatedDate);
         }
 
         [Fact]
-        public void TotalCalculator()
+        public void TotalCalculator_ReturnsQuoteRequestTotals()
         {
+            //Arrange
             var quote = Quote;
             var cartFromQuote = quote.ToCartModel();
             var store = TestStore;
@@ -239,8 +311,10 @@ namespace VirtoCommerce.QuoteModule.Test
                     retVal.DiscountTotal = retVal.SubTotalExlTax * quote.ManualRelDiscountAmount * 0.01m;
                 }
             }
+            //Act
             var sut = new DefaultQuoteTotalsCalculator(storeService.Object);
             var result = sut.CalculateTotals(quote);
+            //Assert
             Assert.Equal(result.GetType(), retVal.GetType());
             var a = Serialize(result);
             var b = Serialize(retVal);
