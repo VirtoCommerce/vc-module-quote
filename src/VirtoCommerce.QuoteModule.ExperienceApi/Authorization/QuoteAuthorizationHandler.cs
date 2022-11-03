7bbbec9449c4bd7c26d903eb8176211c8f4a3bc0
artem-dudarev <ad@virtoway.com>
2022-11-03 11:39:00 +0300

    Allow organization maintainer manage quotes

--- src/VirtoCommerce.QuoteModule.ExperienceApi/Authorization/QuoteAuthorizationHandler.cs
+++ src/VirtoCommerce.QuoteModule.ExperienceApi/Authorization/QuoteAuthorizationHandler.cs
@@ -1,16 +1,37 @@
+using System;
+using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using VirtoCommerce.CustomerModule.Core.Model;
+using VirtoCommerce.CustomerModule.Core.Services;
 using VirtoCommerce.ExperienceApiModule.Core;
 using VirtoCommerce.Platform.Core;
+using VirtoCommerce.Platform.Core.Security;
 using VirtoCommerce.QuoteModule.ExperienceApi.Aggregates;
 using VirtoCommerce.QuoteModule.ExperienceApi.Queries;
 
 namespace VirtoCommerce.QuoteModule.ExperienceApi.Authorization;
 
+public class QuoteAuthorizationRequirement : IAuthorizationRequirement
+{
+}
+
 public class QuoteAuthorizationHandler : AuthorizationHandler<QuoteAuthorizationRequirement>
 {
-    protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, QuoteAuthorizationRequirement requirement)
+    private readonly Func<UserManager<ApplicationUser>> _userManagerFactory;
+    private readonly IMemberService _memberService;
+
+    public QuoteAuthorizationHandler(
+        Func<UserManager<ApplicationUser>> userManagerFactory,
+        IMemberService memberService)
+    {
+        _userManagerFactory = userManagerFactory;
+        _memberService = memberService;
+    }
+
+    protected override async Task HandleRequirementAsync(AuthorizationHandlerContext context, QuoteAuthorizationRequirement requirement)
     {
         var result = context.User.IsInRole(PlatformConstants.Security.SystemRoles.Administrator);
 
@@ -21,7 +42,8 @@ public class QuoteAuthorizationHandler : AuthorizationHandler<QuoteAuthorization
             switch (context.Resource)
             {
                 case QuoteAggregate quote:
-                    result = quote.Model.CustomerId == currentUserId;
+                    result = quote.Model.CustomerId == currentUserId ||
+                        await IsOrganizationMaintainer(context.User, currentUserId, quote.Model.OrganizationId);
                     break;
                 case QuotesQuery query:
                     query.UserId = currentUserId;
@@ -38,10 +60,31 @@ public class QuoteAuthorizationHandler : AuthorizationHandler<QuoteAuthorization
         {
             context.Fail();
         }
-
-        return Task.CompletedTask;
     }
 
+
+    // TODO: Use scoped permission
+    private async Task<bool> IsOrganizationMaintainer(ClaimsPrincipal principal, string userId, string organizationId)
+    {
+        return !string.IsNullOrEmpty(organizationId) &&
+            principal.HasGlobalPermission("xapi:my_organization:edit") &&
+            ((await GetContact(userId))?.Organizations.Contains(organizationId, StringComparer.OrdinalIgnoreCase) ?? false);
+    }
+
+    private async Task<Contact> GetContact(string userId)
+    {
+        Contact contact = null;
+
+        var userManager = _userManagerFactory();
+        var user = await userManager.FindByIdAsync(userId);
+
+        if (!string.IsNullOrEmpty(user?.MemberId))
+        {
+            contact = await _memberService.GetByIdAsync(user.MemberId) as Contact;
+        }
+
+        return contact;
+    }
 
     private static string GetUserId(AuthorizationHandlerContext context)
     {
--- src/VirtoCommerce.QuoteModule.ExperienceApi/Authorization/QuoteAuthorizationRequirement.cs
+++
@@ -1,7 +0,0 @@
-using Microsoft.AspNetCore.Authorization;
-
-namespace VirtoCommerce.QuoteModule.ExperienceApi.Authorization;
-
-public class QuoteAuthorizationRequirement : IAuthorizationRequirement
-{
-}
