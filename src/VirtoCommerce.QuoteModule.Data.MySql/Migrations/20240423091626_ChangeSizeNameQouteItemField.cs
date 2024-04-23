1a28eaca3923d3d7689ad552a884ecb2e19ee4d7
Evgeny Polyakov <47387646+Jounik53@users.noreply.github.com>
2024-04-23 12:45:01 +0300

    VCST-1108: Changing the size of the name quote item field (#112)
    feat: Changing the size of the name quote item field (#112)

---
+++ src/VirtoCommerce.QuoteModule.Data.MySql/Migrations/20240423091626_ChangeSizeNameQouteItemField.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VirtoCommerce.QuoteModule.Data.MySql.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeSizeNameQouteItemField : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "QuoteItem",
+                type: "varchar(1024)",
+                maxLength: 1024,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(256)",
+                oldMaxLength: 256)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "QuoteItem",
+                type: "varchar(256)",
+                maxLength: 256,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(1024)",
+                oldMaxLength: 1024)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
---
+++ src/VirtoCommerce.QuoteModule.Data.PostgreSql/Migrations/20240423091658_ChangeSizeNameQouteItemField.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VirtoCommerce.QuoteModule.Data.PostgreSql.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeSizeNameQouteItemField : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "QuoteItem",
+                type: "character varying(1024)",
+                maxLength: 1024,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(256)",
+                oldMaxLength: 256);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "QuoteItem",
+                type: "character varying(256)",
+                maxLength: 256,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(1024)",
+                oldMaxLength: 1024);
+        }
+    }
+}
