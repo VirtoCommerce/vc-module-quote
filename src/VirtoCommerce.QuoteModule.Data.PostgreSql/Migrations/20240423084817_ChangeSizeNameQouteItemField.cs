57301280ae03959f04c2e73fa739b6ae8678d7af
Evgeny Polyakov <47387646+Jounik53@users.noreply.github.com>
2024-04-23 11:52:29 +0300

    Changing the size of the name quote item field

---
+++ src/VirtoCommerce.QuoteModule.Data.PostgreSql/Migrations/20240423084817_ChangeSizeNameQouteItemField.cs
@@ -0,0 +1,36 @@
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VirtoCommerce.QuoteModule.Data.PostgreSql.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeSizeNameQouteItemField : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "QuoteItem",
+                type: "nvarchar(1024)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(256)",
+                oldNullable: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "QuoteItem",
+                type: "nvarchar(256)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(1024)",
+                oldNullable: false);
+        }
+    }
+}
