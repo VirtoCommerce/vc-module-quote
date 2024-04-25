b6b3a4e2c15b0683de5b5b89774eb3ca6fb07d0d
Basil Kotov <basilkot@gmail.com>
2024-04-25 20:01:31 +0100

    update order with totals

--- src/VirtoCommerce.QuoteModule.ExperienceApi/Commands/ApproveQuoteCommandHandler.cs
+++ src/VirtoCommerce.QuoteModule.ExperienceApi/Commands/ApproveQuoteCommandHandler.cs
@@ -9,11 +9,13 @@ using VirtoCommerce.OrdersModule.Core.Services;
 using VirtoCommerce.QuoteModule.Core;
 using VirtoCommerce.QuoteModule.Core.Models;
 using VirtoCommerce.QuoteModule.Core.Services;
+using static VirtoCommerce.OrdersModule.Core.ModuleConstants;
 
 namespace VirtoCommerce.QuoteModule.ExperienceApi.Commands;
 
 public class ApproveQuoteCommandHandler(
         ICustomerOrderBuilder customerOrderBuilder,
+        ICustomerOrderService customerOrderService,
         IQuoteRequestService quoteRequestService,
         IQuoteTotalsCalculator totalsCalculator)
     : IRequestHandler<ApproveQuoteCommand, ApproveQuoteResult>
@@ -43,6 +45,13 @@ public class ApproveQuoteCommandHandler(
         }
 
         var order = await customerOrderBuilder.PlaceCustomerOrderFromCartAsync(cart);
+
+        order.ShippingTotal = totals.ShippingTotal;
+        order.Total = totals.GrandTotalInclTax;
+        order.TaxTotal = totals.TaxTotal;
+        order.Status = CustomerOrderStatus.NotPayed;
+
+        await customerOrderService.SaveChangesAsync(new[] { order });
         await quoteRequestService.SaveChangesAsync(new[] { quote });
 
         return new ApproveQuoteResult
