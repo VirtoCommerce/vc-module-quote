6337006041ef5576d1d83c45d2267685b9abd319
Alexey Shibanov <83034617+alexeyshibanov@users.noreply.github.com>
2024-05-31 15:42:06 +0300

    feat: Resize address fields according to customer address fields

--- src/VirtoCommerce.QuoteModule.Data.MySql/Migrations/20240531122945_ResizeAddressFields.Designer.cs
+++ src/VirtoCommerce.QuoteModule.Data.MySql/Migrations/20240531122945_ResizeAddressFields.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using VirtoCommerce.QuoteModule.Data.Repositories;
 
@@ -11,9 +12,11 @@ using VirtoCommerce.QuoteModule.Data.Repositories;
 namespace VirtoCommerce.QuoteModule.Data.MySql.Migrations
 {
     [DbContext(typeof(QuoteDbContext))]
-    partial class QuoteDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240531122945_ResizeAddressFields")]
+    partial class ResizeAddressFields
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- src/VirtoCommerce.QuoteModule.Data/Model/AddressEntity.cs
+++ src/VirtoCommerce.QuoteModule.Data/Model/AddressEntity.cs
@@ -7,38 +7,52 @@ namespace VirtoCommerce.QuoteModule.Data.Model
 {
     public class AddressEntity : Entity, IHasOuterId
     {
-        [StringLength(32)]
-        public string AddressType { get; set; }
         [StringLength(64)]
+        public string AddressType { get; set; }
+
+        [StringLength(512)]
         public string Organization { get; set; }
+
         [StringLength(3)]
         public string CountryCode { get; set; }
+
         [Required]
-        [StringLength(64)]
+        [StringLength(128)]
         public string CountryName { get; set; }
+
         [Required]
         [StringLength(128)]
         public string City { get; set; }
+
         [StringLength(64)]
         public string PostalCode { get; set; }
+
         [StringLength(2048)]
         public string Line1 { get; set; }
+
         [StringLength(2048)]
         public string Line2 { get; set; }
+
         [StringLength(128)]
         public string RegionId { get; set; }
+
         [StringLength(128)]
         public string RegionName { get; set; }
+
         [Required]
-        [StringLength(64)]
+        [StringLength(128)]
         public string FirstName { get; set; }
+
         [Required]
-        [StringLength(64)]
+        [StringLength(128)]
         public string LastName { get; set; }
+
         [StringLength(64)]
         public string Phone { get; set; }
-        [StringLength(254)]
+
+        [StringLength(256)]
         public string Email { get; set; }
+
         [StringLength(128)]
         public string OuterId { get; set; }
 
