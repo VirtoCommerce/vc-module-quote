49ddeef80b6a6459404b0939c6f57eba9b6801b0
Konstantin Savosteev <Konstantin.Savosteev@virtoway.com>
2025-06-05 17:31:20 +0200

    fix: sonar

--- src/VirtoCommerce.QuoteModule.Web/Scripts/blades/quote-item-detail.js
+++ src/VirtoCommerce.QuoteModule.Web/Scripts/blades/quote-item-detail.js
@@ -1,10 +1,5 @@
 angular.module('virtoCommerce.quoteModule')
-    .controller('virtoCommerce.quoteModule.quoteItemDetailController', ['$scope',
-        'platformWebApp.bladeNavigationService', 'platformWebApp.settings', 'platformWebApp.dialogService', 'platformWebApp.metaFormsService',
-        'virtoCommerce.quoteModule.quotes', 'virtoCommerce.customerModule.members', 'virtoCommerce.customerModule.memberTypesResolverService',
-        function ($scope, bladeNavigationService, settings, dialogService, metaFormsService, quotes, members, memberTypesResolverService) {
+    .controller('virtoCommerce.quoteModule.quoteItemDetailController', ['$scope', function ($scope) {
             var blade = $scope.blade;
             blade.isLoading = false;
-
-
         }]);
